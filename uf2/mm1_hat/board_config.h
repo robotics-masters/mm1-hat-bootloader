b499ba73c759d98f1a6111f456d46fac7d5724bf
wallarug <cian.byrne@hotmail.com>
2019-03-26 20:59:23 +1100

    updated names.  Please note that VID: 0x0005, PID: 0x0002

--- uf2/mm1_hat/board_config.h
+++ uf2/mm1_hat/board_config.h
@@ -4,8 +4,8 @@
 #define CRYSTALLESS    1
 
 #define VENDOR_NAME "Robotics Masters"
-#define PRODUCT_NAME "MM1 HAT"
-#define VOLUME_LABEL "MM1BOOT"
+#define PRODUCT_NAME "Robo HAT MM1"
+#define VOLUME_LABEL "ROBOBOOT"
 #define INDEX_URL "https://roboticsmasters.co"
 #define BOARD_ID "SAMD21G18A-v2"
 
